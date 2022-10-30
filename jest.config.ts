2e4248652fae53f409223d878d87d1b773ad747b
lens0021 <lorentz0021@gmail.com>
2022-10-30 21:45:52 +0900

    jest: fix testMatch

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'jest';
 
 const config: Config = {
 	preset: 'ts-jest',
-	testMatch: ['server/src/**/?(*.)+(spec|test).[jt]s?(x)'],
+	testMatch: ['**/server/src/**/*.(spec|test).[jt]s?(x)'],
 	testEnvironment: 'node',
 	moduleDirectories: ['node_modules', '<rootDir>'],
 };
