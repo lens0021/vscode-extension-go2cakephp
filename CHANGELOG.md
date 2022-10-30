670b4ae9c09194ab82b6012975d0c849dd6aee26
lens0021 <lorentz0021@gmail.com>
2022-10-30 21:03:38 +0900

    Add CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# CHANGELOG
+
+## Unreleased
+
+- Go 2 CakePHP now searches components on `lib/Cake/Controller/Component/*Component.php` also. (https://github.com/lens0021/vscode-extension-go2cakephp/issues/8)
+
+## v0.1.1
+
+- Extension page changes.
+
+## v0.1.0
--- server/src/parser/word-getter.spec.ts
+++ server/src/parser/word-getter.spec.ts
@@ -5,7 +5,8 @@ describe('', () => {
 	it.each([
 		['echo $this->Foo;', 'echo $this->F'.length],
 		['$this->Foo->someCustomFunction();', '$this->F'.length],
-		["public $uses = ['Foo'];", "public $uses = ['Fo".length]
+		["public $uses = ['Foo'];", "public $uses = ['Fo".length],
+		["public $components = array('Foo');", "public $components = array('F".length],
 	])('%s', (line, pos) => {
 		expect(getWordAt(line, pos)).toBe(word);
 	});
